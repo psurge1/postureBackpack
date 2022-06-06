c395aa013b69c9f3fb4ad34fe7f93505fba11f1b
psurge1 <suraj.sswamy@gmail.com>
2022-06-05 21:40:19 -0500

    calculated the total weight of the circuit

--- weight_calculation.py
+++ weight_calculation.py
@@ -12,13 +12,28 @@ class element:
         else:
             print("ERR: wrong units")
     
-    def get_weight(self, units="grams"):
+    def get_weight(self, units="pounds"):
         return self.weight[units]
+    
+    def __str__(self):
+        return self.get_weight("pounds")
 
 def main():
     battery = element()
-    battery.set_weight(48, "grams")
-    print(battery.get_weight("grams"))
+    battery.set_weight(48)
+    motor = element()
+    motor.set_weight(104)
+    arduino_uno = element()
+    arduino_uno.set_weight(25)
+    wifi_sheild = element()
+    wifi_sheild.set_weight(15)
+    tennis_ball = element()
+    tennis_ball.set_weight(57)
+    accessories_and_wires = element()
+    accessories_and_wires.set_weight(10)
+
+    print(2*(battery.get_weight("grams")+motor.get_weight("grams")+tennis_ball.get_weight("grams"))+arduino_uno.get_weight("grams")+wifi_sheild.get_weight("grams")+accessories_and_wires.get_weight("grams"))
+    print(2*(battery.get_weight()+motor.get_weight()+tennis_ball.get_weight())+arduino_uno.get_weight()+wifi_sheild.get_weight()+accessories_and_wires.get_weight())
 
 if __name__ == '__main__':
     main()
